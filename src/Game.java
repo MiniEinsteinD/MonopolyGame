564b4aea83eb6806dbdcc20951ee194b84c37ec9
Ethan Leir <37488488+ethanleir@users.noreply.github.com>
2021-10-17 21:36:19 -0400

    Created class without implementation.

---
+++ src/Game.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Game {
+    private final ArrayList<Tile> TILES;
+    private Player activePlayer;
+    private ArrayList<Player> players;
+    private TwoDice dice;
+
+    public Game(){
+
+    }
+
+    public void start(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

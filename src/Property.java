cda98493fd9b4dfee8b7d30bfc17a0272d0731d4
flogphindog <91336514+flogphindog@users.noreply.github.com>
2021-10-19 16:12:06 -0400

    Added Tile abstract class functionality and Property class functionality.

--- src/Property.java
+++ src/Property.java
@@ -1,2 +1,48 @@
-public class Property {
+public class Property extends Tile{
+
+    private Player owner;
+    private final String GROUP;
+    private final int PRICE;
+    private boolean owned;
+
+
+    public Property(String name, int price, String group){
+        super(name);
+        this.PRICE = price;
+        this.GROUP = group;
+    }
+
+    /**
+     * Set owner of tile after purchase.
+     * @param p the player purchasing the property
+     */
+    public void setOwner(Player p){
+        this.owner = p;
+        this.owned = true;
+    }
+
+    /**
+     * Get the owner of the property
+     * @return the Player owner of the property
+     */
+    public Player getOwner(){
+        return this.owner;
+    }
+
+    /**
+     * Get true if property has been purchased
+     * @return owned boolean value of property
+     */
+    public boolean isOwned(){
+        return owned;
+    }
+
+    /**
+     * Get the price of the property
+     * @return the property's price
+     */
+    public int getPrice(){
+        return PRICE;
+    }
+
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,17 @@
 public abstract class Tile {
+
+    private final String NAME;
+
+    Tile(String tName){
+        this.NAME = tName;
+    }
+
+    /**
+     * Get the name of the tile object
+     * @return the String name of the Tile
+     */
+    public String getName() {
+        return NAME;
+    }
+
 }

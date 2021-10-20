e7f8dc9af1a9e82806c09839223741f9a78cb4b4
Daniah Mohammed <76576485+MiniEinsteinD@users.noreply.github.com>
2021-10-20 05:10:04 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,38 @@
-public class Player{
+import java.util.ArrayList;
 
+/**
+ * @author Daniah Mohammed
+ * St# 101145902
+ */
+public class Player{
+    private final String id;
+    private int wallet;
+    private ArrayList<Property> properties = new ArrayList<>();
+    private int position;
+    private final String color;
+
+    public Player(String id, String color){
+        this.id = id;
+        this.color=color;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(int wallet) {
+        this.wallet = wallet;
+    }
+
+    public void movePlayer(int steps){
+        this.position += steps;
+    }
 }

c59a670f6829b5fa790fcfef7229ba39107bf64a
flogphindog <91336514+flogphindog@users.noreply.github.com>
2021-10-23 22:00:16 -0400

    Fixed error in equals() override and added documentation to new methods

--- src/Property.java
+++ src/Property.java
@@ -6,7 +6,6 @@ public class Property extends Tile{
     private final String GROUP;
     private final int PRICE;
     private boolean owned;
-    //Daniah Added
     protected static final double FINE_PERCENTAGE = 0.1;
 
 
@@ -58,21 +57,36 @@ public class Property extends Tile{
     }
 
 
-    //Daniah Added this since u were not in the meeting
+    /**
+     * Get string version of the property object
+     * @return String statement about a property tile
+     */
     @Override
     public String toString() {//Make a print statement
         return "\nYou own the following properties: " +  "\n\n    The property name is: " + getName() + "\n    The price of the property is: "+
                 getPrice() + "\n    It is a part from: " + getGroup() +"group";
     }
 
+    /**
+     * Check if another object is the same
+     * @param p, object to be compared
+     * @return true or false depending on the values of the other tile
+     */
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Property property = (Property) o;
-        return PRICE == property.PRICE && owned == property.owned && Objects.equals(owner, property.owner) && Objects.equals(GROUP, property.GROUP);
+    public boolean equals(Object p) {
+        if (p instanceof Property ) {
+            Property o = (Property) p;
+            if (this.PRICE == o.getPrice() && this.GROUP.equals(o.getGroup()) && this.getName().equals(o.getName()) && this.owner == o.getOwner() && this.owned == o.isOwned()) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    /**
+     * Get Property object hashcode
+     * @return hashcode of Property tile
+     */
     @Override
     public int hashCode() {
         return Objects.hash(owner, GROUP, PRICE, owned);

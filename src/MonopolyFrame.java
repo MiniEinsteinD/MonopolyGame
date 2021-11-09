730c493ec970c10abd617ce6e02d4ec0c0dde49a
flogphindog <91336514+flogphindog@users.noreply.github.com>
2021-11-08 20:53:33 -0500

    Routine Commit - MonopolyFrame

--- src/MonopolyFrame.java
+++ src/MonopolyFrame.java
@@ -8,16 +8,15 @@ import java.awt.event.ActionListener;
  */
 public class MonopolyFrame extends JFrame implements MonopolyView{
 
-
     private JFrame numPlayerMenu;
     private JLabel numPanelText;
-
-    private JFrame propertyViewer;
 
     private JButton rollButton;
     private JButton buyButton;
     private JButton passButton;
     private JButton playerOverviewButton;
+    private JButton helpButton;
+
 
     private JLabel walletStateText;
     private JLabel activePlayerText;
@@ -28,31 +27,36 @@ public class MonopolyFrame extends JFrame implements MonopolyView{
     private ImageIcon boardImage;
 
 
-    public MonopolyFrame(){
+    public MonopolyFrame() {
         super("Monopoly6 [TEMP NAME]");
         this.setLayout(new BorderLayout());
         Monopoly model = new Monopoly();
         model.addView(this);
-        this.setSize(new Dimension(1200 ,800));
+        this.setSize(new Dimension(1200, 800));
 
+        //Create Standard font
+        Font stdFont = new Font("Comic Sans MS",Font.BOLD,30);
         //Create Select Player Number Menu
 
         numPlayerMenu = new JFrame("Select Number of Players");
-        numPlayerMenu.setSize(new Dimension(300,600));
+        numPlayerMenu.setSize(new Dimension(300, 600));
         numPlayerMenu.setLayout(new FlowLayout());
 
-        numPanelText = new JLabel("How many people are playing?",SwingConstants.CENTER);
+        numPanelText = new JLabel("How many people are playing?", SwingConstants.CENTER);
+        numPanelText.setFont(stdFont);
         numPlayerMenu.add(numPanelText);
 
-        for (int i = Monopoly.MIN_PLAYERS; Monopoly.MAX_PLAYERS >= i ;i++){
+        for (int i = Monopoly.MIN_PLAYERS; Monopoly.MAX_PLAYERS >= i; i++) {
             JButton numButton = new JButton();
             numButton.setText(Integer.toString(i));
-            numButton.setSize(new Dimension(50,50));
+            numButton.setSize(new Dimension(50, 50));
             int startVal = i;
+            JFrame mainFrameTempVar = this;
             numButton.addActionListener((new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     model.start(startVal);
                     numPlayerMenu.setVisible(false);
+                    mainFrameTempVar.setVisible(true);
                 }
             }));
             numPlayerMenu.add(numButton);
@@ -60,6 +64,7 @@ public class MonopolyFrame extends JFrame implements MonopolyView{
 
 
         MonopolyController rollCont = new RollController(model);
+        MonopolyController helpCont = new HelpController(model);
         MonopolyController buyCont = new BuyController(model);
         MonopolyController passCont = new PassController(model);
         MonopolyController stateCont = new PlayerStateController(model);
@@ -73,19 +78,25 @@ public class MonopolyFrame extends JFrame implements MonopolyView{
         passButton.addActionListener(passCont);
         playerOverviewButton = new JButton("View Player Portfolio");
         playerOverviewButton.addActionListener(stateCont);
+        helpButton = new JButton("Help");
+        helpButton.addActionListener(helpCont);
+
 
         //Panel Initialization
         boardPane = new JPanel();
         infoPane = new JPanel();
-        infoPane.setPreferredSize(new Dimension(500,800));
+        infoPane.setPreferredSize(new Dimension(500, 800));
         boardPane.setLayout(new BorderLayout());
-        infoPane.setLayout(new GridLayout(0,1));
+        infoPane.setLayout(new GridLayout(0, 1));
         boardPane.setBackground(Color.BLACK);
         infoPane.setBackground(Color.YELLOW);
 
         //Label initialization
-        activePlayerText = new JLabel("",SwingConstants.CENTER);
-        walletStateText = new JLabel("",SwingConstants.CENTER);
+
+        activePlayerText = new JLabel("", SwingConstants.CENTER);
+        activePlayerText.setFont(stdFont);
+        walletStateText = new JLabel("", SwingConstants.CENTER);
+        walletStateText.setFont(stdFont);
 
 
         //Adding Board Image
@@ -98,8 +109,10 @@ public class MonopolyFrame extends JFrame implements MonopolyView{
         infoPane.add(walletStateText);
         infoPane.add(rollButton);
         infoPane.add(buyButton);
-        infoPane.add(passButton);
         infoPane.add(playerOverviewButton);
+        infoPane.add(passButton);
+        infoPane.add(helpButton);
+
         boardPane.add(boardMap, BorderLayout.CENTER);
 
 
@@ -108,11 +121,11 @@ public class MonopolyFrame extends JFrame implements MonopolyView{
 
         numPlayerMenu.setDefaultCloseOperation(EXIT_ON_CLOSE);
         numPlayerMenu.pack();
-
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.pack();
-        this.setVisible(true);
+
         numPlayerMenu.setVisible(true);
+
     }
 
 
